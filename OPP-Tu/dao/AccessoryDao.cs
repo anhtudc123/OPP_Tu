8a8c367e2dc2276fb98eb512a9dacaf487a19633
anhtudc123 <116538416+anhtudc123@users.noreply.github.com>
2022-10-31 11:29:42 +0700

    Add files via upload

--- OPP-Tu/dao/AccessoryDao.cs
+++ OPP-Tu/dao/AccessoryDao.cs
@@ -9,13 +9,5 @@ namespace ConsoleApp3.dao
 {
     public class AccessoryDao:BaseDao
     {
-        /// <summary>
-        /// Search for an object in the map by name
-        /// </summary>
-        /// <param name="name"></param> name of choice
-        /// <param name="nameTable"></param> choose one of three table (product, category, accessory)
-        /// <param name="row"></param> alternative 
-        /// <returns></returns>
-       
     }
 }
